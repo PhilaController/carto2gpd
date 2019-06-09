c43ccb47be64c79ede30db3fa3e49fd66eff512d
Nick Hand <nicholas.adam.hand@gmail.com>
2019-06-08 22:44:41 -0400

    fix bug in anaconda script

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -6,10 +6,10 @@ echo "Building conda recipe..."
 conda build conda-recipe
 
 echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
+conda convert --platform all $HOME/miniconda/conda-bld/linux-64/carto2gpd-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/PACKAGENAME-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload conda-bld/**/carto2gpd-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
