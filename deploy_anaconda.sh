bd85f57e097224160d0edddc8bb46718faca9cff
Nick Hand <nicholas.adam.hand@gmail.com>
2019-06-08 23:15:18 -0400

    add skip existing

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -9,7 +9,7 @@ echo "Converting conda package..."
 conda convert --platform all $HOME/miniconda/conda-bld/linux-64/carto2gpd-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/carto2gpd-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload --skip-existing conda-bld/**/carto2gpd-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
