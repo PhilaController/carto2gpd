51baeed0b87a7f9761b0f3bab2c6cf6126b76f73
Nick Hand <nicholas.adam.hand@gmail.com>
2021-10-15 16:45:15 -0400

    fix upload

--- deploy_anaconda.sh
+++ deploy_anaconda.sh
@@ -6,7 +6,7 @@ echo "Building conda recipe..."
 conda build --variants "{python: $PYTHON_VERSIONS}" conda-recipe
 
 echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda/conda-bld/*/carto2gpd-*.tar.bz2 --output-dir conda-bld/
+conda convert --platform all /usr/share/miniconda/conda-bld/linux-64/carto2gpd-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
 anaconda -t $ANACONDA_TOKEN upload --skip-existing conda-bld/**/carto2gpd-*.tar.bz2
