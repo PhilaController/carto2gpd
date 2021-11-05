e8a06216c12f7828560e7508567cd9e4b18356a3
Nick Hand <nicholas.adam.hand@gmail.com>
2021-11-04 21:47:07 -0400

    add support for py37

--- carto2gpd/__init__.py
+++ carto2gpd/__init__.py
@@ -1,4 +1,13 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    from importlib_metadata import version
+
+from .core import get
+
+__version__ = version(__package__)
+
 
 from .core import get, get_size
 
