17e00d728b0e4dbdaac4f8090317d31b54a7e20c
Nick Hand <nicholas.adam.hand@gmail.com>
2022-07-05 12:56:51 -0400

    update dependencies

--- carto2gpd/__init__.py
+++ carto2gpd/__init__.py
@@ -1,14 +1,4 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    # Running on pre-3.8 Python; use importlib-metadata package
-    from importlib_metadata import version
-
-from .core import get
-
-__version__ = version(__package__)
-
-
+from importlib.metadata import version
 from .core import get, get_size
 
 __version__ = version(__package__)
